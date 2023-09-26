e83da84e551b564e57cef0d32da0a8224e167caf
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2023-09-26 12:11:26 +0530

    admin item ratings (currently only mess items) route made

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -5,6 +5,8 @@ import express, { Request, Response, NextFunction } from "express";
 import { userResult } from "../Interface/interfaces";
 import user from "../models/user";
 import complaint from "../models/complaint";
+import mealItem from "../models/mealItem";
+import {all} from "axios";
 
 
 const getUser = async (Email:any):Promise<userResult> =>{
@@ -57,3 +59,43 @@ export const assignRole = async (req:any, res: Response, next: NextFunction)=>{
     }
 }
 
+const getAdminMess = async (Email:any)=>{
+    const userDetails = await user.findOne({Email:Email});
+    if(userDetails){
+        return userDetails.Eating_Mess;
+    }else{
+        return null;
+    }
+}
+
+const getItemName = async (ItemId:any)=>{
+    const foodItem = await mealItem.findById(ItemId);
+    if(foodItem) {
+        return foodItem.Name;
+    }else{
+        return null;
+    }
+}
+
+export const getItemRatings = async (req:any, res:Response,next: NextFunction) => {
+    let data=req.user;
+    try{
+        const adminEatingMess =getAdminMess(data.email);
+        if(adminEatingMess===null){
+            res.send("User not found").status(404);
+        }else{
+            const allMessItems = await menuTable.find({Mess:adminEatingMess});
+            const result=new Set();
+            for(let i=0;i<allMessItems.length;i++){
+                for(let j=0;j<allMessItems[i].Meal_Items.length;j++){
+                    let name = await getItemName(allMessItems[i].Meal_Items[j]);
+                    result.add(name);
+                }
+            }
+            res.send(result).status(200);
+        }
+    }catch (e) {
+        res.send("Unexpected Error").status(501);
+        console.log(e);
+    }
+}
