223c1bbb151cc0fb49c1dba6467685f5f27379b5
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2023-09-26 11:49:46 +0530

    user role route implemented

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,47 +1,59 @@
-// import menuTable from "../models/menuTable";
+import menuTable from "../models/menuTable";
 
-// require("dotenv").config();
-// import express, { Request, Response, NextFunction } from "express";
-// import { userResult } from "../Interface/interfaces";
-// import user from "../models/user";
-// import complaint from "../models/complaint";
+require("dotenv").config();
+import express, { Request, Response, NextFunction } from "express";
+import { userResult } from "../Interface/interfaces";
+import user from "../models/user";
+import complaint from "../models/complaint";
 
-// export const getComplaints = async (req: any, res: Response, next: NextFunction) => {
-//     let data = req.user;
-//     try {
-//         let currUser: userResult = <userResult>(<unknown>user.findOne({ Email: data.email }));
-//         if (!currUser) {
-//             res.status(404).send("User not found");
-//         } else {
-//             let userMess = currUser.Eating_Mess;
-//             let allComplaints = complaint.find({ Mess: userMess });
-//             res.send(allComplaints).status(200);
-//         }
-//     } catch (error) {
-//         res.status(501).send("Some Error orccured");
-//         console.log(error);
-//     }
-// };
 
-// export const foodRating = async (req: any, res: Response, next: NextFunction) => {
-//     let data = req.user;
-//     try{
-//         let currUser: userResult = <userResult>(<unknown>user.findOne({Email: data.email}));
-//         if(!currUser){
-//             res.status(404).send("User not found");
-//         }else{
-//             let userMess = currUser.Eating_Mess;
-//             let allMenus =await menuTable.find({Mess:userMess});
-//             let allItems: any = [];
-//             for (const items in allMenus) {
-//                 for(const item in (<any>items).Meal_Items){
-//                     allItems.concat(item);
-//                 }
-//             }
-//             res.send(allItems).status(200);
-//         }
-//     }catch (err){
-//         console.log(err);
-//         res.status(501).send("Unexpected Error");
-//     }
-// }
+const getUser = async (Email:any):Promise<userResult> =>{
+    return <userResult>(<unknown>user.findOne({Email: Email}));
+}
+
+export const getComplaints = async (req: any, res: Response, next: NextFunction) => {
+    let data = req.user;
+    try {
+        let currUser: userResult = await getUser(data.email);
+        if (!currUser) {
+            res.status(404).send("User not found");
+        } else {
+            let userMess = currUser.Eating_Mess;
+            let allComplaints = complaint.find({ Mess: userMess });
+            res.send(allComplaints).status(200);
+        }
+    } catch (error) {
+        res.status(501).send("Some Error orccured");
+        console.log(error);
+    }
+};
+
+export const getAllUsers = async (req:any, res: Response,next: NextFunction)=>{
+    let data = req.user;
+    try{
+        let currUser: userResult = await  getUser(data.email);
+        if (!currUser) {
+            res.status(404).send("User not found");
+        } else {
+            let userMess = currUser.Eating_Mess;
+            let allUsers = user.find({Mess: userMess});
+            res.send(allUsers);
+        }
+    }catch (err) {
+        res.send("Unexpected Error").status(500);
+        console.log(err);
+    }
+}
+
+export const assignRole = async (req:any, res: Response, next: NextFunction)=>{
+    try{
+        const userId = req.body.userId;
+        const newRole = req.body.newRole;
+        const currUser = await user.findOneAndUpdate({_id:userId},{role:newRole},{new:true});
+        res.send(currUser).status(200);
+    }catch(err){
+        res.send("Unexpected Error").status(501);
+        console.log(err);
+    }
+}
+
